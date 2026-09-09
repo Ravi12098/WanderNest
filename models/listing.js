be121e2cb92527e7599a309e35aa5e9e0bcedbc2
Ravinder Saharan <saharanravi823@gmail.com>
2026-09-09 17:27:46 +0530

    sample listing

--- models/listing.js
+++ models/listing.js
@@ -2,13 +2,24 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title: String,
+    title: {
+      type: String,
+      required: true,
+    },
     description: String,
-    image: String,
+    image: {
+      type: String,
+      default: 
+        "https://tse2.mm.bing.net/th/id/OIP.P-gYsqgJDg7RvTaaG1sIWwHaJ4?r=0&pid=Api&P=0&h=180",
+      set: (v) => 
+        v === ""
+          ? "https://tse2.mm.bing.net/th/id/OIP.P-gYsqgJDg7RvTaaG1sIWwHaJ4?r=0&pid=Api&P=0&h=180" 
+          : v,
+    },
     price: Number,
     location: String,
     country: String,
 });
 
 const Listing = mongoose.model("listing", listingSchema);
-modules.export = Listing;
+module.exports = Listing;
