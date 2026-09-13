0fd3d44e5c65255c22ff487c00318c77f0945ed6
Ravinder Saharan <saharanravi823@gmail.com>
2026-09-13 16:48:02 +0530

    index route

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/test/WanderNest";
+const MONGO_URL = "mongodb://127.0.0.1:27017/WanderNest";
 
 main()
   .then(() => {
@@ -18,6 +18,11 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({
+      ...obj,
+     image: obj.image.url,
+    }));
+
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 };
