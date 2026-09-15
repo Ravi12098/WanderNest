ecd42ad50e38dddcdc3abd6c7f07592e1801fcf7
Ravinder Saharan <saharanravi823@gmail.com>
2026-09-15 16:47:01 +0530

    create (new & create route)

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ app.get("/listings", async (req, res) => {
   res.render("listings/index.ejs", {allListings});
 });
 
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
 app.get("/listings/:id", async (req, res) => {
   let {id} = req.params;
   const listing = await Listing.findById(id);
