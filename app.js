24a5131813bd57f7fca85db6a3b8f70304190876
Ravinder Saharan <saharanravi823@gmail.com>
2026-09-20 16:40:42 +0530

    delete route

--- app.js
+++ app.js
@@ -61,6 +61,13 @@ app.put("/listings/:id", async (req, res) => {
   res.redirect(`/listings/${id}`);
 });
 
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
+
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
 //       title: "My New Villa",
